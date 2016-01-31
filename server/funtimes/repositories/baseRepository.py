bf8baae5c10ce94677191ef6bbf18ce71132aec4
Alex Bettadapur <alexbettadapur@gmail.com>
2016-01-31 16:02:22 -0500

    Implemented add function in base repository

--- server/funtimes/repositories/baseRepository.py
+++ server/funtimes/repositories/baseRepository.py
@@ -1,18 +1,21 @@
 from abc import ABCMeta, abstractmethod
 from funtimes.models.base import BaseModel
+from funtimes import db
 
 class BaseRepository(metaclass=ABCMeta):
 
     def __init__(self, model_class: BaseModel):
         self.model_class = model_class
 
+    # Get an entity by its primary key
     def find(self, id):
         entity = self.model_class.query.get(id)
         return entity
 
+    # Add an entity if it doesn't exist, or update an existing entity
     def add_or_update(self, entity):
         if self.validate(entity):
-            pass
+            db.session.add(entity)
         else:
             pass
 
@@ -22,4 +25,7 @@ class BaseRepository(metaclass=ABCMeta):
     @abstractmethod
     def validate(self, entity):
         pass
+
+    def save_changes(self):
+        db.session.commit()
 
--- server/funtimes/repositories/itineraryRepository.py
+++ server/funtimes/repositories/itineraryRepository.py
@@ -8,6 +8,9 @@ class ItineraryRepository(BaseRepository):
     def __init__(self):
         super(ItineraryRepository, self).__init__(Itinerary)
 
+    def add_or_update(self, entity):
+        super(ItineraryRepository, self).add_or_update(entity)
+
     def validate(self, entity):
         return True
 
