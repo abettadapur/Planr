da34d0adc1758957ffd05adafb98cec603cf3072
Alex Bettadapur <alexbettadapur@gmail.com>
2016-02-02 16:23:56 -0500

    Overhauled database migrations architecture

--- server/funtimes/__init__.py
+++ server/funtimes/__init__.py
@@ -3,12 +3,13 @@ import os
 from flask import Flask, render_template
 from flask_restful import Api
 from flask.ext.sqlalchemy import SQLAlchemy
+from funtimes.etc import config
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'app.db')
-app.config['SQLALCHEMY_MIGRATE_REPO'] = os.path.join(basedir, 'db_repository')
+app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_MIGRATE_REPO'] = config.SQLALCHEMY_MIGRATE_REPO
 
 db = SQLAlchemy(app)
 
--- server/manage.py
+++ server/manage.py
@@ -1,40 +1,18 @@
-import argparse
-
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
 from funtimes import db
+from funtimes import app
+from funtimes.etc import config
 from funtimes.models.itinerary import Itinerary
 from funtimes.models.item import Item
 from funtimes.models.user import User
 from funtimes.models.category import Category
 from funtimes.models.search_filter import SearchFilter
 
+migrate = Migrate(app, db)
 
-def create_db():
-    db.create_all()
-
-
-def drop_db():
-    db.drop_all()
-
-
-def main():
-    parser = argparse.ArgumentParser(
-        description='Manage this Flask application.')
-    parser.add_argument(
-        'command', help='the name of the command you want to run')
-    parser.add_argument(
-        '--seedfile', help='the file with data for seeding the database')
-    args = parser.parse_args()
-
-    if args.command == 'create_db':
-        create_db()
-
-        print("DB created!")
-    elif args.command == 'delete_db':
-        drop_db()
-
-        print("DB deleted!")
-    else:
-        raise Exception('Invalid command')
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
-    main()
+    manager.run()
