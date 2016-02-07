4fe8200c60a64e69735e6f35bcffa44290c4ee13
andrey <andrey.sendme@gmail.com>
2016-02-07 04:46:44 -0500

    Adding baby Android project

---
+++ android/funtimes/app/src/androidTest/java/thingswithworth/org/funtimes/ApplicationTest.java
@@ -0,0 +1,13 @@
+package thingswithworth.org.funtimes;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
