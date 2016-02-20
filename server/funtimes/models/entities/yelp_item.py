1d5083e525128961f156305c76da11e8242cb9a1
Alex Bettadapur <alexbettadapur@gmail.com>
2016-02-19 19:42:13 -0800

    Added coordinate entity

--- server/funtimes/models/entities/yelp_item.py
+++ server/funtimes/models/entities/yelp_item.py
@@ -1,7 +1,19 @@
-from funtimes.models.entities.location import Location
+from funtimes.models.entities.yelp_location import YelpLocation
+from funtimes import db
+from funtimes.models.entities.base import BaseModel
 
 
 class YelpItem(object):
+    __tablename__ = "yelp_item"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    name = db.Column(db.String(200), nullable=False)
+    image_url = db.Column(db.String(300), nullable=True)
+    url = db.Column(db.String(300), nullable=True)
+    phone = db.Column(db.String(15), nullable=True)
+    rating = db.Column(db.Integer())
+    yelp_location_id = db.Column(db.Integer, db.ForeignKey("yelp_location.id"))
+    yelp_location = db.relationship("YelpLocation")
+
     def __init__(self, id=None, name=None, image_url=None, url=None, phone=None, rating=1, location=None):
         self.id = id
         self.name = name
@@ -20,6 +32,6 @@ class YelpItem(object):
             url=dict['url'],
             phone=dict['phone'],
             rating=dict['rating'],
-            location=Location.create_from_dict(dict['location'])
+            location=YelpLocation.create_from_dict(dict['location'])
         )
         return item
---
+++ server/funtimes/models/entities/yelp_location.py
@@ -0,0 +1,29 @@
+from funtimes import db
+from funtimes.models.entities.coordinate import Coordinate
+from funtimes.models.entities.base import BaseModel
+
+
+class YelpLocation(BaseModel):
+
+    id = db.Column(db.Integer(), primary_key=True)
+    address = db.Column(db.String(500), nullable=False)
+    city = db.Column(db.String(100), nullable=False)
+    postal_code = db.Column(db.String(100), nullable=False)
+    coordinate_id = db.Column(db.Integer, db.ForeignKey("coordinate.id"))
+    coordinate = db.relationship("Coordinate")
+
+    def __init__(self, address, city, postal_code, coordinate):
+        self.address = address
+        self.city = city
+        self.postal_code = postal_code
+        self.coordinate = coordinate
+
+    @staticmethod
+    def create_from_dict(create_dict):
+        location = YelpLocation(
+            address=create_dict['address'],
+            city=create_dict['city'],
+            postal_code=create_dict['postal_code'],
+            coordinate=Coordinate(create_dict['coordinate']['latitude'], create_dict['coordinate']['longitude'])
+        )
+        return location
