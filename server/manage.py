0deb192deb7b77a1e81b7eefe3e845e28a566083
Alex Bettadapur <alexbettadapur@gmail.com>
2016-02-19 20:03:37 -0800

    Updated model index

--- server/manage.py
+++ server/manage.py
@@ -2,6 +2,16 @@ from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 from funtimes import app
 from funtimes import db
+from funtimes.models.entities.base import BaseModel
+from funtimes.models.entities.item import Item
+from funtimes.models.entities.coordinate import Coordinate
+from funtimes.models.entities.itinerary import Itinerary
+from funtimes.models.entities.user import User
+from funtimes.models.entities.user_authorization import UserAuthorization
+from funtimes.models.entities.yelp_item import YelpItem
+from funtimes.models.entities.yelp_location import YelpLocation
+from funtimes.models.entities.yelp_search_filter import YelpSearchFilter
+from funtimes.models.entities.yelpcategory import YelpCategory
 
 migrate = Migrate(app, db)
 
