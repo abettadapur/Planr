b05264f213fcaf66f537b64cf55bf7e9e5b7cf43
Alex Bettadapur <alexbettadapur@gmail.com>
2016-03-11 14:46:46 -0500

    Added some intro icons

--- android/PlanFun/app/src/main/java/devpost/yelp/planfun/ui/activities/PlansIntro.java
+++ android/PlanFun/app/src/main/java/devpost/yelp/planfun/ui/activities/PlansIntro.java
@@ -1,5 +1,6 @@
 package devpost.yelp.planfun.ui.activities;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import com.github.paolorotolo.appintro.AppIntro2;
@@ -14,10 +15,10 @@ public class PlansIntro extends AppIntro2 {
     @Override
     public void init(Bundle savedInstanceState) {
 
-        addSlide(AppIntroFragment.newInstance("Welcome to Plans", "Plans is a new way to find things to do in your city", R.drawable.planfun, R.color.colorPrimary));
-        addSlide(AppIntroFragment.newInstance("Choose a city and go!", "Choose any starting location and have our tool find things to do around you", R.drawable.map_marker_radius, R.color.colorPrimary));
-        addSlide(AppIntroFragment.newInstance("The more the merrier", "Share your created plan with your Facebook friends to make coordinating easier", R.drawable.planfun, R.color.colorPrimary));
-        addSlide(AppIntroFragment.newInstance("Stay Flexible", "Movie night at a friend's? Birthday dinner? Add your own custom events to make a plan truly yours", R.drawable.planfun, R.color.colorPrimary));
+        addSlide(AppIntroFragment.newInstance("Welcome to Plans", "Plans is a new way to find things to do in your city", R.drawable.planfun, R.color.primaryColor));
+        addSlide(AppIntroFragment.newInstance("Choose a city and go!", "Choose any starting location and have our tool find things to do around you", R.drawable.city, R.color.primaryColor));
+        addSlide(AppIntroFragment.newInstance("The more the merrier", "Share your created plan with your Facebook friends to make coordinating easier", R.drawable.share, R.color.primaryColor));
+        addSlide(AppIntroFragment.newInstance("Stay Flexible", "Movie night at a friend's? Birthday dinner? Add your own custom events to make a plan truly yours", R.drawable.map_search, R.color.primaryColor));
         setFlowAnimation();
     }
 
